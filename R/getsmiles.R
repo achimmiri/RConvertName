f084a0543356404a221d173b1e076cbae09a9d9a
Ahusha Chimmiri <achimmiri@ipb-halle.de>
2023-03-08 12:30:03 +0100

    this adds name to smiles function

---
+++ R/getsmiles.R
@@ -0,0 +1,13 @@
+getsmiles-function(getNAME)
+{
+  ###Baicalin
+  ###################################	
+  url<- "http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"
+  out<-tryCatch({jsonlite::fromJSON(paste0(url,getNAME, "/property/InChIKey"))},error = function(x) {return(NA)})
+  out1<-tryCatch({jsonlite::fromJSON(paste0(url,getNAME, "/property/CanonicalSMILES"))}, error = function(x) {return(NA)})
+  out2<-tryCatch({jsonlite::fromJSON(paste0(url,getNAME, "/property/InChI"))}, error = function(x) {return("NA")})
+  ############## SMILES
+  OSM<-tryCatch({out1$PropertyTable$Properties$CanonicalSMILES},error=function(cond){return(NA)})
+  #######################
+  return(OSM)
+}
